44759f445797bffc9fdf54d4ced3a626ae71f238
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-12-09 17:43:50 +0000

    enums in java

---
+++ Day-02/enumeration/EnumDemo1.java
@@ -0,0 +1,18 @@
+enum Status {
+    Success, Failed, Loading, Crash; // objects or Named Constants
+}
+
+public class EnumDemo1 {
+    public static void main(String[] args) {
+        Status s = Status.Success;
+        System.out.println(s);
+        System.out.println(s.ordinal());
+
+        System.out.println(Status.Loading.ordinal());
+
+        Status[] all = Status.values();
+        for (Status status : all) {
+            System.out.println(status + " : " + status.ordinal());
+        }
+    }
+}

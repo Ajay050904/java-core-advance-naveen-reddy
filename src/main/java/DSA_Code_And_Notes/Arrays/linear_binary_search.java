f7b101726c8a20130d0e3e3af3dc491cb4a22f96
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2026-01-14 15:49:42 +0000

    searching

---
+++ src/main/java/DSA_Code_And_Notes/Arrays/linear_binary_search.java
@@ -0,0 +1,42 @@
+package DSA_Code_And_Notes.Arrays;
+
+public class linear_binary_search {
+
+    // linear search
+    public static int linearSearch(int[] nums, int target) {
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] == target) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    // binary serach - works on sorted arrays only
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 5, 7, 9, 11, 13 };
+        int target = 111;
+        int result = linearSearch(nums, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}

51177883ae614da23b3a7178030618012a22cff8
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2026-01-15 17:51:42 +0000

    second maximum element

---
+++ src/main/java/DSA_Code_And_Notes/Arrays/Arrays_Questions/second_largest.java
@@ -0,0 +1,27 @@
+package DSA_Code_And_Notes.Arrays.Arrays_Questions;
+
+import java.util.Scanner;
+
+public class second_largest {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        int smax = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > smax && arr[i] != max)
+                smax = arr[i];
+        }
+        System.out.println(smax);
+        sc.close();
+    }
+}

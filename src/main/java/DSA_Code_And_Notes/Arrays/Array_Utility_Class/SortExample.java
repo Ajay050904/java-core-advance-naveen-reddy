175b6b810ad68f0dd980299d6e89fd6e4c5039f2
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2026-01-20 17:48:36 +0000

    sort() method

---
+++ src/main/java/DSA_Code_And_Notes/Arrays/Array_Utility_Class/SortExample.java
@@ -0,0 +1,20 @@
+package DSA_Code_And_Notes.Arrays.Array_Utility_Class;
+
+import java.util.Arrays;
+
+public class SortExample {
+    public static void main(String[] args) {
+
+        int[] arr = { 5, 2, 8, 1, 3 };
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
+/*
+ * Arrays.sort()
+ * `````````````
+ * Purpose: Sorts the array in ascending order
+ * ````````
+ */

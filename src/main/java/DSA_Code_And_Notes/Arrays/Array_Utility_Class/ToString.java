3012ab623548ec21fecfbb12c6857531ec1f1d63
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2026-01-20 17:51:57 +0000

    toString() method

---
+++ src/main/java/DSA_Code_And_Notes/Arrays/Array_Utility_Class/ToString.java
@@ -0,0 +1,16 @@
+package DSA_Code_And_Notes.Arrays.Array_Utility_Class;
+
+import java.util.Arrays;
+
+public class ToString {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30 };
+        System.out.println(Arrays.toString(arr));
+    }
+}
+/*
+ * Arrays.toString()
+ * `````````````````
+ * Purpose: Converts array into readable string format
+ * ````````
+ */
